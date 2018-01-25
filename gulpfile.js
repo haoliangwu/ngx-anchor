2bed14c26dac8feef8f55cd802f8fa7d4f3f0235
Lyon <wuhaoliang@cloudist.cc>
2018-01-25 10:27:11 +0800

    [IMP] gulp deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
-const gulp = require('gulp');
-const { inlineResourcesForDirectory } = require('./inline-resources');
-const sass = require('gulp-sass');
+const gulp = require('gulp')
+const { inlineResourcesForDirectory } = require('./inline-resources')
+const sass = require('gulp-sass')
 const args = require('get-gulp-args')()
+const ghPages = require('gulp-gh-pages')
 
 const moduleType = args.module
 
@@ -12,25 +13,33 @@ const SOURCE_PATH = {
 }
 const TARGET_PATH = moduleType === 'es2015' ? './publish/ngx-anchor' : './publish/es5/ngx-anchor'
 
-gulp.task('copy-and-inline-resource', copyHtml);
-
 function copyHtml() {
   gulp.src(SOURCE_PATH.HTML)
-    .pipe(gulp.dest(TARGET_PATH)).on('end', copyAssets);
+    .pipe(gulp.dest(TARGET_PATH)).on('end', copyAssets)
 }
 
 function copyAssets() {
   gulp.src(SOURCE_PATH.ASSET)
-    .pipe(gulp.dest(TARGET_PATH)).on('end', copyScss);
+    .pipe(gulp.dest(TARGET_PATH)).on('end', copyScss)
 }
 function copyScss() {
   gulp.src(SOURCE_PATH.SCSS)
     .pipe(sass())
-    .pipe(gulp.dest(TARGET_PATH)).on('end', inlineResource);
+    .pipe(gulp.dest(TARGET_PATH)).on('end', inlineResource)
 }
 
 function inlineResource() {
-  inlineResourcesForDirectory(TARGET_PATH);
+  inlineResourcesForDirectory(TARGET_PATH)
 }
 
-gulp.task('compile', ['copy-and-inline-resource']);
+gulp.task('copy-and-inline-resource', copyHtml)
+
+gulp.task('compile', ['copy-and-inline-resource'])
+
+
+function deploy() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages())
+}
+
+gulp.task('deploy', deploy)
--- src/ngx-anchor/model.ts
+++ src/ngx-anchor/model.ts
@@ -1,4 +1,5 @@
 export interface Anchor {
+  uuid: number,
   id: string,
   el: HTMLElement,
   text?: string,
